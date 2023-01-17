5548797188b89b1fe2a2a2d472e7a2c9c568ef2a
Laís Galvão <laisgbueno62@gmail.com>
2023-01-17 13:21:25 -0300

    atualizando enunciado

--- index.ts
+++ index.ts
@@ -1,14 +1,12 @@
 // Import stylesheets
 import './style.css';
 
-// Abaixo temos uma lista de estudantes, a faculdade precisa selecionar alguns dados dessa lista. Para cada um dos itens abaixo, crie uma função em typescript que receba a lista de alunos e que retorne o que a faculdade precisa:
+// No exercício abaixo temos uma lista de estudantes, a faculdade precisa selecionar alguns dados dessa lista. Para cada um dos itens abaixo, crie uma função em typescript que receba a lista de alunos e que retorne o que a faculdade precisa:
 //  - Uma lista contendo a cidade e quantos alunos são dessa cidade
 //  - A média de idade dos alunos
 //  - A porcentagem de alunos acima de 21 anos
 //  - Uma lista de clubes baseados nos interesses dos alunos (ex: Leitura, Jogos, Dança) contendo todos os alunos com o interesse em comum.
 
-// Não se preocupe com apresentação do resultado das funções, elas serão avaliadas a nível de lógica, práticas de código e legibilidade.
-// Se sinta livra para usar bibliotecas úteis para iteração dos dados.
 
 type Student = {
   id: number;
